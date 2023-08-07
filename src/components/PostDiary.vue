d80eaf3eccda09dd91ac60e5933b9ebb9d96156b
Riki Sato <belutuno6463@gmail.com>
2023-08-07 15:08:16 +0000

    投稿機能を作成

--- src/components/PostDiary.vue
+++ src/components/PostDiary.vue
@@ -1,37 +1,40 @@
 <template>
    <div class= "ui main container">
     <div class="ui segment">
-    <h1>今日の日記</h1>
+    <h1 class="custom-h1">今日の日記</h1>
     
-      <form class="ui form" >
-    <h2>悲しかったこと</h2>
+      <form class="ui massive form" >
+    <h2 class="custom-h2">悲しかったこと</h2>
         <div class="field">
           <div class="ui input">
-            <textarea rows="4" type="text" v-model="post.text1" ></textarea>
+            <textarea class="custom-textarea" rows="4" type="text" v-model="post.text1"  placeholder="(例) 朝寝坊した"></textarea>
           </div><!--ui input-->
         </div><!--field-->
         
         
-    <h2>嬉しかったこと</h2>
+    <h2 class="custom-h2">嬉しかったこと</h2>
         <div class="field">
           <div class="ui input">
-            <textarea rows="4" type="text" v-model="post.text2" ></textarea>
+            <textarea class="custom-textarea" rows="4" type="text" v-model="post.text2" placeholder="(例) ご飯がおいしかった"></textarea>
           </div><!--ui input-->
         </div><!--field-->
         
         
-    <h2>明日の目標</h2>
+    <h2 class="custom-h2">明日の目標</h2>
         <div class="field">
           <div class="ui input">
-            <textarea rows="4" type="text" v-model="post.text3"  ></textarea>
+            <textarea class="custom-textarea" rows="4" type="text" v-model="post.text3" placeholder="(例) 散歩する" ></textarea>
           </div><!--ui input-->
         </div><!--field-->
         
-    
-          <div class="field">
-            <button @click="postRequest()" class="ui  blue  button right floated" type="submit">投稿</button>
-          </div><!--field-->
       </form> <!-- ui form-->
+      
+     <div class="ui segment"> <!-- 追加: 新しいsegment -->  
+        <div class="field">
+            <button @click="postRequest()" class="ui  massive fluid button custom-color" type="submit">投稿</button>
+          </div><!--field-->
+    </div><!--ui segment-->
+    
     </div> <!--ui segment-->
   </div> <!--ui main container-->
  
@@ -62,6 +65,7 @@ export default {
         text1:null,
         text2:null,
         text3:null,
+        userId:window.localStorage.getItem('userId')
       }
     };
   },
@@ -115,6 +119,44 @@ export default {
 </script>
 
 <style scoped>
+.ui.input{
+    border: 3px solid #4DC4FF; /* 縁の色を赤に設定 */
+    border-radius: 15px; /* 縁を丸くする半径の値を設定（10pxは例です） */
+    overflow: hidden;  /*子要素をまるく*/
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.5); /* ボックスシャドウを追加 */
+  }
+  
+
+.custom-color {
+    background-color: #4DC4FF;
+    color: white;
+  }
+  
+  /* ボタンが押された状態のスタイル */
+  .custom-color:active,
+  .custom-color:hover,
+  .custom-color:focus{
+    background-color: #4DC4FF;
+    color: white;
+  }
+  
+  /* h1要素のカスタムスタイル */
+  .custom-h1 {
+    font-size: 45px; /* 好みに応じて大きさを調整 */
+    color: #333; /* 好みに応じて文字色を設定 */
+    text-align: center; /* 好みに応じてテキストの位置を調整 */
+  }
+
+  /* h1要素のカスタムスタイル */
+  .custom-h2 {
+    font-size: 36px; /* 好みに応じて大きさを調整 */
+    color: #333; /* 好みに応じて文字色を設定 */
+  }
+  
+  .custom-textarea{
+    font-size:33px;
+  }
+
 
 
 </style>
