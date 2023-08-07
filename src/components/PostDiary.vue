36190f35581501b39d9805532280904b48d972e3
Riki Sato <belutuno6463@gmail.com>
2023-08-07 06:32:02 +0000

    postDiaryファイルを追加

---
+++ src/components/PostDiary.vue
@@ -0,0 +1,46 @@
+<template>
+  <router-view/>
+ 
+</template>
+
+<script>
+// 必要なものはここでインポートする
+// @は/srcと同じ意味です
+// import something from '@/components/something.vue';
+// import { baseUrl } from '@/assets/config.js';
+
+// const headers = {'Authorization' : 'mtiToken'};
+
+import {baseUrl} from '@/assets/config.js';
+
+export default {
+  name: 'Home',
+
+  components: {
+   // 読み込んだコンポーネント名をここに記述する
+ 
+  },
+
+  data() {
+    // Vue.jsで使う変数はここに記述する
+    return {
+     
+    };
+  },
+  created() {
+  
+  },
+  computed: {
+  // 計算した結果を変数として利用したいときはここに記述する
+  },
+
+
+  methods: {
+   
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
